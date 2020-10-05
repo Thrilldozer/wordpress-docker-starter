be6ebfc79e67c7c182e1ea2cb3b66de6eb20c415
Blake Pfaff <blake.a.pfaff2@gmail.com>
2020-10-05 14:17:08 -0500

    adds footer and header nemus

--- app/wp-content/themes/mytheme/page.php
+++ app/wp-content/themes/mytheme/page.php
@@ -1,12 +1,15 @@
 <?php get_header();?>
 
-<div class="container">
+<section class="page-wrap">
 
-  <h1><?php the_title();?></h1>
+  <div class="container">
 
-  <?php get_template_part('includes/section', 'content');?>
+    <h1><?php the_title();?></h1>
 
-</div>
+    <?php get_template_part('includes/section', 'content');?>
 
+  </div>
+
+</section>
 
 <?php get_footer();?>
--- app/wp-content/themes/mytheme/template-contactus.php
+++ app/wp-content/themes/mytheme/template-contactus.php
@@ -7,22 +7,26 @@ Template Name: Contact Us
 
 <?php get_header();?>
 
-<div class="container">
+<section class="page-wrap">
 
-  <h1><?php the_title();?></h1>
+  <div class="container">
 
-  <div class="row">
-  
-    <div class="col-lg-6">
-      this is where the contact form goes
-    </div>
-    <div class="col-lg-6">
-      <?php get_template_part('includes/section', 'content');?>
+    <h1><?php the_title();?></h1>
+
+    <div class="row">
+    
+      <div class="col-lg-6">
+        this is where the contact form goes
+      </div>
+      <div class="col-lg-6">
+        <?php get_template_part('includes/section', 'content');?>
+      </div>
+
     </div>
 
   </div>
 
-</div>
+</section>
 
 
 <?php get_footer();?>
